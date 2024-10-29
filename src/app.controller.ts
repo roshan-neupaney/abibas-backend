2e6cd2e91c2608f8795d5b2a6d17ec76f0f6266c
Roshan Neupane <roshanneupane600@gmail.com>
2024-10-29 13:25:47 +0545

    added vercel.json

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ApiTags } from '@nestjs/swagger';
 
 @Controller()
-@ApiTags('users')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'this.appService.getHello()';
   }
 }
